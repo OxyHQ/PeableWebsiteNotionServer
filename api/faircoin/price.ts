3d205f4c50bfbc13dfa926a3afc79acb6cc294ab
Nate Isern Alvarez <albert.isern.alvarez@gmail.com>
2024-07-31 19:26:57 +0200

    Update price.ts

--- api/faircoin/price.ts
+++ api/faircoin/price.ts
@@ -22,7 +22,6 @@ const price = getMinuteBasedRandomPrice(basePrice, variationFactor);
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   res.setHeader("Content-Type", "application/json");
-  res.setHeader("Access-Control-Allow-Methods", "GET, PUT, DELETE, OPTIONS");
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://fairco.in/");
   return res.status(200).send(JSON.stringify({ price: price }, null, 2));
 }
